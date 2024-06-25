e7f3e5ff285ad916e63bde799d3d47091cd24a65
Code Faisal <126379981+CodeFaisalDev@users.noreply.github.com>
2024-06-25 21:03:41 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -120,7 +120,7 @@ const Navbar = () => {
               <FiShoppingCart size={24} />
             </Link>
             {totalItems > 0 && (
-              <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">
+              <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-red-600 rounded-full">
                 {totalItems}
               </span>
             )}
